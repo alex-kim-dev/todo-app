2064f788653ee1c0ca3ffc2ddcc436a9253937ee
Alex Kim <45559664+alex-kim-dev@users.noreply.github.com>
2022-01-06 10:05:57 +0500

    feat: set up starter

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()]
-})
+export default defineConfig(({ mode }) => ({
+  base: mode === 'production' ? '/todo-app/' : '/',
+
+  root: 'src',
+
+  build: {
+    emptyOutDir: true,
+    sourcemap: true,
+  },
+
+  plugins: [react()],
+}));
