8d2ccd5093f2f68d59ba1eeeb34c3613a8875038
Alex Kim <45559664+alex-kim-dev@users.noreply.github.com>
2022-03-26 17:35:27 +0500

    feat: add drag & drop mechanics to the todo items

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,107 +1,42 @@
+import {
+  closestCenter,
+  DndContext,
+  DragEndEvent,
+  DragOverlay,
+  DragStartEvent,
+  KeyboardSensor,
+  MouseSensor,
+  TouchSensor,
+  useSensor,
+  useSensors,
+} from '@dnd-kit/core';
+import {
+  SortableContext,
+  sortableKeyboardCoordinates,
+  verticalListSortingStrategy,
+} from '@dnd-kit/sortable';
 import styled from '@emotion/styled';
+import { useState } from 'react';
 
 import { deleteTodo, toggleTodoCompletion } from '../actions';
-import { srOnly } from '../GlobalCss';
 import { useGlobalState } from '../GlobalState';
-import { Filters, Id } from '../types';
-import Checkmark from './Checkmark';
+import { Filters, Id, ITodo } from '../types';
+import SortableTodoItem from './SortableTodoItem';
+import TodoItem from './TodoItem';
+
+const activationConstraint = { delay: 300, tolerance: 5 };
+
+const emptyTodo: ITodo = {
+  id: '0',
+  task: '',
+  completed: false,
+};
 
 const List = styled.ul`
   list-style: none;
   margin: 0;
   padding: 0;
 `;
-
-const Item = styled.li(
-  ({ theme: { mq, palette } }) => `
-    display: flex;
-    align-items: center;
-    padding: 1.6rem 2rem;
-    box-sizing: content-box;
-    border-bottom: 0.1rem solid ${palette.muted};
-
-    & > :not(:last-child) {
-      margin-right: 1.2rem;
-    }
-
-    ${mq.smUp} {
-      padding: 1.8rem 2.2rem;
-
-      & > :not(:last-child) {
-        margin-right: 1.8rem;
-      }
-
-      &:hover > button {
-        background-image: url('./icon-cross.svg');
-      }
-    }
-
-    ${mq.mdUp} {
-      padding: 2rem 2.4rem;
-
-      & > :not(:last-child) {
-        margin-right: 2.4rem;
-      }
-    }
-  `,
-);
-
-const Task = styled.p<{ completed?: boolean }>(
-  ({ theme: { mq, palette }, completed }) => `
-    flex: 1;
-    font-size: 1.2rem;
-
-    ${
-      completed
-        ? `
-          text-decoration: line-through;
-          color: ${palette.textSecondary};
-        `
-        : ''
-    }
-
-    ${mq.smUp} {
-      font-size: 1.5rem;
-    }
-
-    ${mq.mdUp} {
-      font-size: 1.8rem;
-    }
-  `,
-);
-
-const DeleteBtn = styled.button(
-  ({ theme: { mq, palette } }) => `
-    --size: 1.2rem;
-    width: 3.4rem;
-    height: 3.4rem;
-    border: 0;
-    border-radius: 0.4rem;
-    padding: 0;
-    margin: calc((3.4rem - var(--size)) / -2);
-    background: center / var(--size) no-repeat url('./icon-cross.svg');
-    cursor: pointer;
-    transition: background-color 0.15s;
-
-    &:hover,
-    &:active {
-      background-color: ${palette.bgHover};
-    }
-
-    &:focus {
-      background-image: url('./icon-cross.svg');
-    }
-
-    ${mq.smUp} {
-      --size: 1.5rem;
-      background-image: none;
-    }
-
-    ${mq.mdUp} {
-      --size: 1.8rem;
-    }
-  `,
-);
 
 const matchTodo = {
   [Filters.all]: () => true,
@@ -111,34 +46,64 @@ const matchTodo = {
 
 const TodoList: React.FC = () => {
   const [{ todos, todosFilter }, dispatch] = useGlobalState();
+  const [draggedTodoId, setDraggedTodoId] = useState<Id | null>(null);
 
-  const handleCheckmarkChange = (id: Id) => (): void => {
-    dispatch(toggleTodoCompletion(id));
-  };
-
-  const handleDeleteClick = (id: Id) => (): void => {
-    dispatch(deleteTodo(id));
-  };
-
+  const draggedTodo = todos.find(({ id }) => id === draggedTodoId) ?? emptyTodo;
+  const todosOrder = todos.map(({ id }) => id);
   const filteredTodos = todos.filter(({ completed }) =>
     matchTodo[todosFilter](completed),
   );
 
+  const sensors = useSensors(
+    useSensor(MouseSensor, { activationConstraint }),
+    useSensor(TouchSensor, { activationConstraint }),
+    useSensor(KeyboardSensor, {
+      coordinateGetter: sortableKeyboardCoordinates,
+    }),
+  );
+
+  const handleComplete = (id: Id) => (): void => {
+    dispatch(toggleTodoCompletion(id));
+  };
+
+  const handleDelete = (id: Id) => (): void => {
+    dispatch(deleteTodo(id));
+  };
+
+  const handleDragStart = (event: DragStartEvent): void => {
+    const { active } = event;
+    setDraggedTodoId(active.id);
+  };
+
+  const handleDragEnd = (event: DragEndEvent): void => {
+    setDraggedTodoId(null);
+  };
+
   return (
     <List id='todo-list' aria-label='Todo list'>
-      {filteredTodos.map(({ id, task, completed }) => (
-        <Item key={id}>
-          <Checkmark checked={completed} onChange={handleCheckmarkChange(id)} />
-          <Task completed={completed}>{task}</Task>
-          <DeleteBtn
-            type='button'
-            onClick={handleDeleteClick(id)}
-            aria-controls='todo-list'
-          >
-            <span css={srOnly}>Delete task</span>
-          </DeleteBtn>
-        </Item>
-      ))}
+      <DndContext
+        sensors={sensors}
+        collisionDetection={closestCenter}
+        onDragStart={handleDragStart}
+        onDragEnd={handleDragEnd}
+      >
+        <SortableContext
+          items={todosOrder}
+          strategy={verticalListSortingStrategy}
+        >
+          {filteredTodos.map((todo) => (
+            <SortableTodoItem
+              key={todo.id}
+              todo={todo}
+              onComplete={handleComplete(todo.id)}
+              onDelete={handleDelete(todo.id)}
+            />
+          ))}
+        </SortableContext>
+        <DragOverlay>
+          {draggedTodoId && <TodoItem todo={draggedTodo} />}
+        </DragOverlay>
+      </DndContext>
     </List>
   );
 };
